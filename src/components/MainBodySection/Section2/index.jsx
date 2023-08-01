3c49bd2cfe9b99826676a0c8bc08e5cfdf31ec2a
daheeda <a01a05@naver.com>
2023-08-01 12:56:09 +0900

    feat : section2 swiper

--- src/components/MainBodySection/Section2/index.jsx
+++ src/components/MainBodySection/Section2/index.jsx
@@ -2,6 +2,16 @@ import { useEffect, useState } from "react";
 import "../MainBodySection.css";
 import IMG from "../../../assets/images/man in front of st basil 20.jpg";
 
+// npm install swiper@6.0.2
+// npm install --save sass
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination , Autoplay, EffectCoverflow} from "swiper";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/navigation/navigation.scss";
+SwiperCore.use([Autoplay]);
+SwiperCore.use([Navigation, Pagination])
+
 export const Section2 = () => {
   //Each React jsx file returns HTML COMPONENTS like below
 
@@ -41,9 +51,25 @@ export const Section2 = () => {
           유명한 관광명소에 직접 가지 않아도 text prompt를 통해 <br></br>
           구체적으로 배경의 형태를 변경할 수 있습니다.</p>
         </div>
-        <div class="temp-box">
-          <img src={IMG} className="section-img" />
-        </div>
+        <Swiper
+            className="temp-box"
+            spaceBetween={50}
+            slidesPerView={1}
+            loop = {true}
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 3000,
+              disableOnInteraction: false }}	// 추가
+          >
+            <SwiperSlide><img src={IMG} className="section-img" />
+            <p> 1 </p>
+            </SwiperSlide>
+            <SwiperSlide><img src={IMG} className="section-img" />
+            <p> 2 </p>
+            </SwiperSlide>
+            <SwiperSlide><img src={IMG} className="section-img" />
+            <p> 3 </p>
+            </SwiperSlide>
+          </Swiper>
       </div>
     </main>
     
