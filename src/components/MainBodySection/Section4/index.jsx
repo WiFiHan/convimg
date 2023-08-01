a688b06bdae12b003d56e3f0ec66987877834749
daheeda <a01a05@naver.com>
2023-08-01 15:30:04 +0900

    사이즈 고정

--- src/components/MainBodySection/Section4/index.jsx
+++ src/components/MainBodySection/Section4/index.jsx
@@ -6,17 +6,20 @@ export const Section4 = () => {
   //Each React jsx file returns HTML COMPONENTS like below
 
   return (
-    <main>
-      <div class="main-container-one">
-        <div class="temp-box">
-          <p class="font-size-30"><br></br>잘 나온 예시들을 참고하여,<br></br>
-          멋진 이미지를 만들어보세요!</p><br></br>
-          <img src={IMG} className="section-img" /><br></br>
-          <p>photo of woman, sitting in the terrace cafe at night, <br>
-          </br>a crossant on the table, with a view of the Eiffel Tower in Paris</p>
+    <div className='section' style={{
+    left : "0%", transform: "translate(0%, 180%)"}}>
+      <div className='content'>
+        <div className='main-container-one'>
+          <div className='temp-box'>
+            <p class="font-size-30"><br></br>잘 나온 예시들을 참고하여,<br></br>
+            멋진 이미지를 만들어보세요!</p><br></br>
+            <img src={IMG} className="section-img" /><br></br>
+            <p>photo of woman, sitting in the terrace cafe at night, <br>
+            </br>a crossant on the table, with a view of the Eiffel Tower in Paris</p>
+          </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
