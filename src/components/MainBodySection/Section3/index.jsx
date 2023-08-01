0577958272e86c5cd713b9461c0d9b5478185d38
daheeda <a01a05@naver.com>
2023-08-01 12:56:37 +0900

    feat : section3 swiper

--- src/components/MainBodySection/Section3/index.jsx
+++ src/components/MainBodySection/Section3/index.jsx
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react";
 import "../MainBodySection.css";
 import IMG from "../../../assets/images/man in front of st basil 20.jpg";
 
+// npm install swiper@6.0.2
+// npm install --save sass
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination , Autoplay, EffectCoverflow} from "swiper";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/navigation/navigation.scss";
+SwiperCore.use([Autoplay]);
+SwiperCore.use([Navigation, Pagination])
 
 export const Section3 = () => {
   //Each React jsx file returns HTML COMPONENTS like below
@@ -9,9 +18,23 @@ export const Section3 = () => {
   return (
     <main>
       <div class="main-container">
-        <div class="temp-box">
-          <img src={IMG} className="section-img" />
-        </div>
+          <Swiper
+            className="temp-box"
+            spaceBetween={50}
+            slidesPerView={1}
+            loop = {true}
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 3000,
+              disableOnInteraction: false }}	// 추가
+          >
+            <SwiperSlide><img src={IMG} className="section-img" />
+            <p> 원본 </p>
+            </SwiperSlide>
+            <SwiperSlide><img src={IMG} className="section-img" />
+            <p> 마스크 </p>
+            </SwiperSlide>
+          </Swiper>
+
         <div class="temp-box">
           <br></br>
           <p class="font-size-30 text_left"><br></br><br></br><br></br>간편한 마스크 수정!</p>
