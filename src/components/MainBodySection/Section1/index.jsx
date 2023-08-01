225b7114aef7ff773e4b6b4be2b0f6224369de06
WiFiHan <mukss0909056@gmail.com>
2023-08-01 13:45:59 +0900

    [feat] Now image data could be uploaded normally

--- src/components/MainBodySection/Section1/index.jsx
+++ src/components/MainBodySection/Section1/index.jsx
@@ -3,7 +3,7 @@ import { Modal } from "../../Modal";
 import "../MainBodySection.css";
 
 export const Section1 = () => {
-  const [files, setFiles] = useState([])
+  const [files, setFiles] = useState([]);
   const [Dragging, setDragging] = useState(false);
   const handleDrop = (event) => {
     setDragging(false);
@@ -11,19 +11,39 @@ export const Section1 = () => {
     const { files } = event.dataTransfer;
     if (files.length > 0) {
       setFiles([...files]);
+      const file = files[0];
+      uploadFile(file);
     }
-  }
+  };
+
+  const uploadFile = (file) => {
+    const reader = new FileReader();
+    reader.onload = function () {
+      const uploadingImage = reader.result;
+      sessionStorage.setItem("uploadedImage", uploadingImage);
+    };
+    reader.readAsDataURL(file);
+  };
+
   const handleDragOver = (event) => {
     setDragging(true);
-    event.preventDefault()
-  }
+    event.preventDefault();
+  };
+
+  const handleUploadClick = (event) => {
+    const file = event.target.files[0];
+    const temp_files = [];
+    temp_files.push(file);
+    setFiles(temp_files);
+    if (file) uploadFile(file);
+  };
 
   const handleDragLeave = (event) => {
     if (event.currentTarget.contains(event.relatedTarget)) return;
     setDragging(false);
-    event.preventDefault()
-  }
-  
+    event.preventDefault();
+  };
+
   const [isModalOpen, setModalOpen] = useState(false);
 
   const openModal = () => {
@@ -36,11 +56,25 @@ export const Section1 = () => {
     console.log(isModalOpen);
   };
 
-
   return (
-    <div className="section" onDragOver={handleDragOver} onDrop={handleDrop} onDragLeave={handleDragLeave}> 
-      <div className={Dragging ? 'uploading' : 'none'}>
-        <div className="main-font" style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)'}}>파일을 여기에 놓으세요</div>
+    <div
+      className="section"
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      onDragLeave={handleDragLeave}
+    >
+      <div className={Dragging ? "uploading" : "none"}>
+        <div
+          className="main-font"
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,-50%)",
+          }}
+        >
+          파일을 여기에 놓으세요
+        </div>
       </div>
       <div className="content">
         <div className="bg-img">
@@ -48,17 +82,21 @@ export const Section1 = () => {
             <div>
               <div className="main-font">이미지 배경 변환기</div>
               <div className="sub-font">사진을 다양한 배경으로 바꿔보세요</div>
-              <div className="sub-font">배경 변환을 위해 이미지를 업로드하세요</div>
+              <div className="sub-font">
+                배경 변환을 위해 이미지를 업로드하세요
+              </div>
             </div>
             <div class="filebox">
               <div class="line">
-                <label for="file" className="box-font">이미지 업로드</label> 
-                <input type="file" id="file"/>
+                <label for="file" className="box-font">
+                  이미지 업로드
+                </label>
+                <input type="file" id="file" onChange={handleUploadClick} />
               </div>
             </div>
             <ul>
               {files.map((file, index) => (
-              <li key={index}>{file.name}</li>
+                <li key={index}>{file.name}</li>
               ))}
             </ul>
           </div>
