c660b4cb840990863030c823612fff380f7f2798
kkIIun <leekijung@igijeong-ui-MacBookPro.local>
2023-08-05 18:41:17 +0900

    fix section3 img2

--- src/components/MainBodySection/Section3/index.jsx
+++ src/components/MainBodySection/Section3/index.jsx
@@ -7,11 +7,11 @@ export const Section3 = () => {
   //Each React jsx file returns HTML COMPONENTS like below
 
   return (
-    <div className='section' style={{height:'900px', marginTop: '300px'}}>
+    <div className='section' style={{height:'900px', marginTop: '200px'}}>
       <div className='content'>
         <div className='main-container-2' style={{marginBottom:'100px'}}>
           <div className='temp-box'>
-            <p className="font-size-30"><br></br><br></br><br></br>다양한 배경으로 변환!</p>
+            <p className="font-size-30"><br></br><br></br><br></br>다양한 배경으로 변환</p>
             <p ><br></br>에펠탑, 경복궁, 콜로세움, 런던아이 등등 <br></br> 유명한 관광명소에 직접 가지 않아도
             text를 통해 <br></br> 구체적으로 배경의 형태를 변경할 수 있습니다. </p>
           </div>
@@ -41,15 +41,10 @@ export const Section3 = () => {
             <img src={IMG} className="section-img" />
           </div>
           <div className='temp-box'>
-            <p className="font-size-30"><br></br><br></br><br></br>간편한 마스크 수정!</p>
-            <p ><br></br>원하는 인물 마스크가 나오지 않았다면, <br></br>
-            직접 클릭을 통해 설정해 보세요. </p>
+            <p className="font-size-30"><br></br><br></br><br></br>완벽한 인물 마스크</p>
+            <p ><br></br>추가로 입력을 하지 않고도 배경과 <br></br>
+            전경을 구분하여 마스크를 만듭니다.</p>
           </div>
-        </div>
-        <div className='container' style={{marginTop: "15px", marginBottom:'100px'}}>
-              <p style={{margin: "0 30px 0"}}></p>
-              <p style={{margin: "0 0px 0"}}>photo of woman,photo of woman, <br></br> photo of woman</p>
-              <p style={{margin: "0 160px 0"}}></p>
         </div>
       </div>
     </div>
